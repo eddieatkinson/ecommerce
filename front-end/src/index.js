d7943941951836fd32f006288b65ecffcc65e786
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-12-12 10:10:42 -0500

    Redux-setup

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import Student from './Student';
+
+// Get the createStore method as well as the applyMiddleware method from redux.
+import { createStore, applyMiddleware } from 'redux';
+
+// createStore needs a reducer. More specifically, a root reducer.
+import RootReducer from './reducers/RootReducer';
+
+// We are going to need AJAX a lot. We will use it in our
+// redux actions which means... we need redux-promise.
+import reduxPromise from 'redux-promise';
 
 ReactDOM.render(
 	<App />,
---
+++ front-end/src/reducers/RootReducer.js
@@ -0,0 +1,22 @@
+// This is the master or root reducer.
+// Each reducer contains a piece of state.
+// The root reducer contains all the reducers.
+// I.e., the root reducer contains ALL pieces of state,
+// or the entire application state.
+
+// In order to get all the "little" reducers or pieces of state
+// into one big, "root" reducer, we need the combineReducers method from redux.
+import { combineReducers } from 'redux';
+
+// Import each individual reducer to hand to combineReducers.
+// First: AuthReducer
+import AuthReducer from './AuthReducer';
+
+// combineReducers taks an object as an arg.
+// That arg has key: value pair = stateName: reducerFunction.
+// The reducerFunction will return a value.
+const rootReducer = combineReducers({
+	auth: AuthReducer
+});
+
+export default rootReducer;
